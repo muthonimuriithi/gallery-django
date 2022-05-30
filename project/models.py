cbac53839989aabf95dea641735a06e0cc8f319a
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-30 17:23:46 +0300

    ft: created a category class and added it to our database

--- project/models.py
+++ project/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+class Location(models.Model):
+    location_name=models.CharField(max_length=30, unique=True )
+
+    def __str__(self):
+        return self.location_name
+
+    def save_location(self):
+        self.save()
+
+class Category(models.Model):
+    category_name=models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.category_name
+
+    def save_category(self):
+        self.save()
+
+
