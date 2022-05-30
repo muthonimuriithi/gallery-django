d95427f65be8fa1dc6fdc37f843c089d7e353308
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-30 17:55:29 +0300

    fx: deleted import of CharField from models

--- project/models.py
+++ project/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.forms import CharField
+
 
 # Create your models here.
 
@@ -20,5 +20,48 @@ class Category(models.Model):
 
     def save_category(self):
         self.save()
+
+class Image(models.Model):
+    image_name = models.CharField(max_length =30)
+    image_description = models.TextField()
+    image_path = models.ImageField(upload_to = 'pictures/')
+    image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add = True,)
+
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+
+    @classmethod
+    def search_category(cls,search_term):
+        search_result = cls.objects.filter(image_category__category_name__icontains=search_term)
+        return search_result
+
+    @classmethod
+    def filter_location(cls,location):
+        filter_loc = cls.objects.filter(image_location__location_name__icontains=location)
+        return filter_loc
+
+
+    @classmethod
+    def get_image_by_id(cls,input_id):
+        image_got = cls.objects.get(id=input_id)
+        return image_got
+
+
+    @classmethod
+    def retrieve_all(cls):
+        all_images = Image.objects.all()
+        return all_images
+        # for item in all_objects:
+        #     return item;
+
 
 
