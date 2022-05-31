1b4e7c1c5550607a761a5c9084b0a64a50c1d51a
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-31 14:25:17 +0300

    ft: added an import of static from urls

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns=[
-    path('',views.index,name='home-page'),
-    
+
+from . import views
+
+
+urlpatterns = [
+    path('',views.landing,name='landing'),
+    path('search/',views.search_results, name='search_results'),
+    path('category/(\w+)', views.page_category,name='page_category'),
+    path('location/(\w+)', views.page_location,name='page_location'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
