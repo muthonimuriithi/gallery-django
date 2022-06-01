08b401d8a188d051a9a5ad6bd9b775bce4b6e814
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-06-01 22:14:31 +0300

    modified path urls

--- project/urls.py
+++ project/urls.py
@@ -10,8 +10,8 @@ from . import views
 urlpatterns = [
     path('',views.landing,name='landing'),
     path('search/',views.search_results, name='search_results'),
-    path('category/(\w+)', views.page_category,name='page_category'),
-    path('location/(\w+)', views.page_location,name='page_location'),
+    path('categories/<str:category>/', views.page_category,name='page_category'),
+    path('locations/<str:location>/', views.page_location,name='page_location'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
